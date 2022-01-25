e020c45b12f4e16fadf07fb4a834d4cdc3f5ef2f
unknown <fajarmh2@gmail.com>
2022-01-25 13:24:25 +0700

    UI XML onboarding 3 & Onboarding Activity on manifest

---
+++ app/src/main/java/com/fajar/mov/onboarding/OnboardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.fajar.mov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fajar.mov.R
+
+class OnboardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three)
+    }
+}
