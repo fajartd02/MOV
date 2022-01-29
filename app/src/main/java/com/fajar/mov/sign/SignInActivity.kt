258696f461d1c54711e2818b24e9f623516760c2
unknown <fajarmh2@gmail.com>
2022-01-30 01:35:21 +0700

    Setting firebase tapi masih error lol

--- app/src/main/java/com/fajar/mov/sign/SignInActivity.kt
+++ app/src/main/java/com/fajar/mov/sign/SignInActivity.kt
@@ -2,11 +2,28 @@ package com.fajar.mov.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.fajar.mov.R
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+
+
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val button_sign = findViewById<Button>(R.id.button_sign)
+        button_sign.setOnClickListener {
+            // Write a message to the database
+            // Write a message to the database
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
