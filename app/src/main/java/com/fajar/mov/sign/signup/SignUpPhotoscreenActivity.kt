468150efa2335a83d0e011e3cf5ac6d9fddaafbb
unknown <fajarmh2@gmail.com>
2022-02-28 15:27:29 +0700

    Add sign up photo + bug when upload

--- app/src/main/java/com/fajar/mov/sign/signup/SignUpPhotoscreenActivity.kt
+++ app/src/main/java/com/fajar/mov/sign/signup/SignUpPhotoscreenActivity.kt
@@ -1,14 +1,155 @@
 package com.fajar.mov.sign.signup
 
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.fajar.mov.HomeActivity
 import com.fajar.mov.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import java.util.*
+import java.util.jar.Manifest
+import java.util.prefs.Preferences
 
-class SignUpPhotoscreenActivity : AppCompatActivity() {
+class SignUpPhotoscreenActivity : AppCompatActivity(), PermissionListener {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    var statusAdd: Boolean = false
+    lateinit var filePath: Uri
+
+    lateinit var storage: FirebaseStorage
+    lateinit var storageReferensi: StorageReference
+    lateinit var preferences: com.fajar.mov.utils.Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_photoscreen)
 
+        var tv_hello = findViewById<TextView>(R.id.tv_hello)
+        var iv_add = findViewById<ImageView>(R.id.iv_add)
+        var btn_save = findViewById<Button>(R.id.btn_save)
+        var iv_profile = findViewById<ImageView>(R.id.iv_profile)
+        var btn_home = findViewById<Button>(R.id.btn_home)
 
+        preferences = com.fajar.mov.utils.Preferences(this)
+        storage = FirebaseStorage.getInstance()
+        storageReferensi = storage.getReference()
+
+        tv_hello.text = "Selamat Datang\n" + intent.getStringArrayExtra("nama")
+
+        iv_add.setOnClickListener {
+            if (statusAdd) {
+                statusAdd = false
+                btn_save.visibility = View.VISIBLE
+                iv_add.setImageResource(R.drawable.ic_btn_upload)
+                iv_profile.setImageResource(R.drawable.user_picture)
+            } else {
+                Dexter.withActivity(this).withPermission(android.Manifest.permission.CAMERA)
+                    .withListener(this)
+                    .check()
+            }
+        }
+        btn_home.setOnClickListener {
+            finishAffinity()
+
+            var goHome = Intent(this@SignUpPhotoscreenActivity, HomeActivity::class.java)
+            startActivity(goHome)
+        }
+
+        btn_save.setOnClickListener {
+            if (filePath != null) {
+                var progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Uploading...")
+                progressDialog.show()
+
+                var ref = storageReferensi.child("images/" + UUID.randomUUID().toString())
+                ref.putFile(filePath)
+                    .addOnSuccessListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(this, "Upload Sucessfully", Toast.LENGTH_LONG).show()
+
+                        ref.downloadUrl.addOnSuccessListener {
+                            preferences.setValues("url", it.toString())
+                        }
+
+                        finishAffinity()
+                        var goHome = Intent(this@SignUpPhotoscreenActivity, HomeActivity::class.java)
+                        startActivity(goHome)
+                    }
+                    .addOnCanceledListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(this, "Upload Failed", Toast.LENGTH_LONG).show()
+                    }
+                    .addOnProgressListener {
+                        taskSnapshot -> var progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
+                        progressDialog.setMessage("Upload: " + progress.toInt() + " %")
+                    }
+
+            } else {
+
+            }
+        }
+
+    }
+
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+            takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+        Toast.makeText(this, "You can't upload the photos profile", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: PermissionRequest?,
+        token: PermissionToken?
+    ) {
+
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "Tergesah? click tombol upload nanti saja", Toast.LENGTH_LONG).show()
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var iv_profile = findViewById<ImageView>(R.id.iv_profile)
+        var btn_save = findViewById<Button>(R.id.btn_save)
+        var iv_add = findViewById<ImageView>(R.id.iv_add)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            var bitmap = data?.extras?.get("data") as Bitmap
+            statusAdd = true
+
+            filePath = data.getData()!!
+            Glide.with(this)
+                .load(bitmap)
+                .apply(RequestOptions.circleCropTransform())
+                .into(iv_profile)
+
+            btn_save.visibility = View.VISIBLE
+            iv_add.setImageResource(R.drawable.ic_btn_delete)
+        }
     }
 }
